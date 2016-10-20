eb5bbc0d2197749950278067881b105130516dc1
Johannes Holmberg <johannes@update.uu.se>
2016-10-20 21:18:48 +0200

    Handle pausing more appropriately

--- server/control.py
+++ server/control.py
@@ -3,26 +3,37 @@ import os
 
 import kakraplay
 
+def wait_for_all(events):
+    """Wait for all Event objects to be true.
+       If any Event is false, wait for it, then check all Events again"""
+    while True:
+        for x in events:
+            if not x.is_set():
+                x.wait()
+                break
+        else:
+            return
+
 
 class Control(threading.Thread):
 
     def __init__(self, kpool, kqueue):
         self.pool = kpool
         self.queue = kqueue
-        self.running = False
         self.item = None
         self.song_id = 0
         self.player = None
-        self.paused = False
+        self.running = threading.Event()
         super(Control, self).__init__()
 
     def run(self):
-        self.running = True
-        while self.running:
-            item_id = None
-            while self.running and item_id is None:
-                self.queue.nonempty.wait(1)
-                item_id = self.queue.get_first()
+        item_id = None
+        self.running.set()
+        while True:
+            wait_for_all([self.running, self.queue.nonempty])
+            item_id = self.queue.get_first()
+            if item_id is None:
+                continue
             self.item = self.pool.get_item(item_id)
 
             for song in self.item.songs:
@@ -31,15 +42,18 @@ class Control(threading.Thread):
                 self.player = kakraplay.get_player(realfilename,
                                                    subtune=song.subtune,
                                                    loops=song.loops)
+                # What if pause has been called by now!? Make this stuff thread safe!
                 self.player.play(block=True)
             self.pool.remove_item(item_id)
             self.queue.pop()
 
     def pause(self):
+        self.running.clear()
         if self.player:
             self.player.pause()
 
     def resume(self):
+        self.running.set()
         if self.player:
             self.player.resume()
 
