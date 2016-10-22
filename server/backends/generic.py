77deacc5b2b473c2e2693c1be192417a80e8dbb5
Johannes Holmberg <johannes@update.uu.se>
2016-10-23 00:58:54 +0200

    Fix bug in loop handling and limit NSFs to 2 loops by default

--- server/backends/generic.py
+++ server/backends/generic.py
@@ -7,7 +7,7 @@ regexps = {
     '.*Vorbis audio.*' : ('ogg123', ['/usr/bin/ogg123'], None, None),
     '.*PlaySID.*' : ('sidplay2', ['/usr/bin/aoss', '/usr/bin/padsp', '/usr/bin/sidplay2', '-os'],
                      '-o$s', None),
-    '.*NES Sound File.*' : ('nosefart', ['/usr/bin/nosefart', '-l 1200]'], '-t $s', '-a $l'),
+    '.*NES Sound File.*' : ('nosefart', ['/usr/bin/nosefart', '-l 1200', '-a 2'], '-t $s', '-a $l'),
     '.*MPEG.*' : ('mplayer', ['/usr/bin/mplayer'], None, None)
 }
 
@@ -27,7 +27,7 @@ class Player(object):
         if subtune:
             self.cmd.append(s.replace('$s', str(subtune)))
         if loops:
-            self.cmd.append(s.replace('$l', str(loops)))
+            self.cmd.append(l.replace('$l', str(loops)))
         print(self.cmd)
         self.cmd.append(self.filename)
         
