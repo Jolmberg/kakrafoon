7f50fec0511e938828ffee1534e7fe54a95dd1a3
Johannes Holmberg <johannes@update.uu.se>
2017-03-01 00:11:50 +0100

    Make dequeue work, and not require a user parameter

--- server/kakqueue.py
+++ server/kakqueue.py
@@ -29,6 +29,9 @@ class _QueueItem(object):
         self.user = user
         self.songs = songs
 
+    def __repr__(self):
+        return "<_Queue_item obj: %s, user: %s, songs: %d>"%(self.obj, self.user, self.songs)
+
 
 class Queue(object):
     def __init__(self):
@@ -37,6 +40,7 @@ class Queue(object):
         self.user_skip = {}
         self.nonempty = threading.Event()
         self.lock = threading.Lock()
+        self.obj_item = {}
 
     def _purge_user(self, user):
         """Remove all traces of a user from the queue"""
@@ -54,6 +58,7 @@ class Queue(object):
             self.user_queue[user] = []
             self.user_skip[user] = 0
         self.user_queue[user].append(item)
+        self.obj_item[obj] = item
         self.nonempty.set()
 
     @synchronised
@@ -67,12 +72,13 @@ class Queue(object):
     @synchronised
     def get_all(self):
         """Get all queue items, in the correct order"""
-        order = self.user_order[:]
+        order = self.user_order.copy()
         position = dict([(u, 0) for u in order])
         skip = self.user_skip.copy()
         queue = []
-        remove = None
+
         while order:
+            remove = set()
             for x in order:
                 if skip[x] > 0:
                     skip[x] -= 1
@@ -83,24 +89,24 @@ class Queue(object):
                         skip[x] = item.songs - 1
                         queue.append(item.obj)
                     else:
-                        remove = x
+                        remove.add(x)
 
-            if remove:
+            for x in remove:
                 order.remove(x)
-                remove = None
         return queue
 
     @synchronised
-    def dequeue(self, user, obj):
+    def dequeue(self, obj):
         """Remove an item from the queue
 
         The next item in the affected user's queue will take its place.
         """
         try:
-            self.user_queue[user].remove(obj)
+            item = self.obj_item.pop(obj)
+            self.user_queue[item.user].remove(item)
             r = True
-            if self.user_skip[user] == 0 and not self.user_queue[user]:
-                self._purge_user(user)
+            if self.user_skip[item.user] == 0 and not self.user_queue[item.user]:
+                self._purge_user(item.user)
                 self._normalise()
         except:
             r = False
@@ -127,6 +133,7 @@ class Queue(object):
             if self.user_skip[x] > 0:
                 continue
             item = self.user_queue[x].pop(0)
+            self.obj_item.pop(item.obj)
             self.user_skip[x] = item.songs
             break
         self._normalise()
@@ -135,25 +142,25 @@ class Queue(object):
 
 def _make_queue():
     q = Queue()
-    q.enqueue('A1', 'A', 1, None)
-    q.enqueue('A1', 'A', 1, None)
-    q.enqueue('A2', 'A', 2, None)
-    q.enqueue('B3', 'B', 3, None)
-    q.enqueue('B1', 'B', 1, None)
-    q.enqueue('C2', 'C', 2, None)
-    q.enqueue('C2', 'C', 2, None)
+    q.enqueue('Ax1', 'A', 1, None)
+    q.enqueue('Ay1', 'A', 1, None)
+    q.enqueue('Az2', 'A', 2, None)
+    q.enqueue('Bx3', 'B', 3, None)
+    q.enqueue('By1', 'B', 1, None)
+    q.enqueue('Cx2', 'C', 2, None)
+    q.enqueue('Cy2', 'C', 2, None)
     return q
 
 
 class QueueTest(unittest.TestCase):
-    def test1(self):
+    def test_basic(self):
         q = _make_queue()
         o = q.get_all()
-        self.assertEqual(o, ['A1','B3','C2','A1','A2','C2','B1'])
+        self.assertEqual(o, ['Ax1','Bx3','Cx2','Ay1','Az2','Cy2','By1'])
 
-    def test2(self):
+    def test_pop(self):
         q = _make_queue()
-        order = ['A1','B3','C2','A1','A2','C2','B1']
+        order = ['Ax1','Bx3','Cx2','Ay1','Az2','Cy2','By1']
         while order:
             self.assertEqual(q.get_all(), order)
             self.assertEqual(q.get_first(), order[0])
@@ -164,6 +171,12 @@ class QueueTest(unittest.TestCase):
         self.assertEqual(q.user_skip, {})
         self.assertEqual(q.user_queue, {})
 
+    def test_dequeue(self):
+        q = _make_queue()
+        q.dequeue('Ax1')
+        self.assertEqual(q.get_all(), ['Ay1','Bx3','Cx2','Az2','Cy2','By1'])
+        q.dequeue('Bx3')
+        self.assertEqual(q.get_all(), ['Ay1','By1','Cx2','Az2','Cy2'])
 
 if __name__ == '__main__':
     unittest.main()
