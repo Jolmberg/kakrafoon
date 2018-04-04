73b69d520d54039e49ca7be773f8dca23e25ad56
Johannes Holmberg <johannes@update.uu.se>
2018-04-04 23:51:56 +0200

    Fix the control loop
    - At least attempt to be thread safe
    - Allow multiple skips while paused

--- server/control.py
+++ server/control.py
@@ -3,75 +3,128 @@ import os
 
 import kakraplay
 
-def wait_for_all(events):
-    """Wait for all Event objects to be true.
-       If any Event is false, wait for it, then check all Events again"""
-    while True:
-        for x in events:
-            if not x.is_set():
-                x.wait()
-                break
-        else:
-            return
+def synchronised(f):
+    """Synchronisation decorator"""
+
+    def real_function(*args, **kw):
+        args[0].lock.acquire()
+        try:
+            return f(*args, **kw)
+        finally:
+            args[0].lock.release()
+    return real_function
 
 
 class Control(threading.Thread):
 
     def __init__(self, kpool, kqueue):
+        self.lock = threading.Lock()
         self.pool = kpool
         self.queue = kqueue
         self.item = None
+        self.songs = []
         self.song = None
+        self.paused = False
         self.player = None
+        self.playing = False
         self.running = threading.Event()
         self.skip_current_item = False
+        self.skip_current_song = False
         super(Control, self).__init__()
 
+    @synchronised
+    def _get_next(self):
+        if len(self.songs) > 1:
+            self.songs.pop(0)
+        else:
+            if self.item:
+                self.pool.remove_item(self.item.key)
+                self.queue.pop()
+            self.song = None
+            self.songs = []
+            self.item = None
+            item_id = self.queue.get_first()
+            if item_id is None:
+                return False
+            self.item = self.pool.get_item(item_id)
+            self.songs = self.item.songs
+        print(self.songs)
+        self.song = self.songs[0]
+        realfilename = os.path.join('/tmp/kakrafoon', str(self.item.key), str(self.song.key))
+        print(realfilename)
+        self.player = kakraplay.get_player(realfilename,
+                                           subtune=self.song.subtune,
+                                           loops=self.song.loops)
+        return True
+
     def run(self):
-        item_id = None
         self.running.set()
         while True:
-            self.item = None
-            wait_for_all([self.queue.nonempty])
-            # The first item cannot be dequeued so we can assume that it exists here:
-            item_id = self.queue.get_first()
-            self.item = self.pool.get_item(item_id)
+            if self._get_next() == False:
+                self.queue.nonempty.wait()
+                continue
 
-            for song in self.item.songs:
-                self.song = song
-                realfilename = os.path.join('/tmp/kakrafoon',str(self.item.key), str(song.key))
-                print(realfilename)
-                self.player = kakraplay.get_player(realfilename,
-                                                   subtune=song.subtune,
-                                                   loops=song.loops)
-                wait_for_all([self.running])
-                self.player.play()
-                self.player = None
-                if self.skip_current_item:
-                    break
-            self.pool.remove_item(item_id)
-            self.skip_current_item = False
-            self.queue.pop()
+            if self.paused:
+                self.running.clear()
+            self.running.wait()
 
+            self.lock.acquire()
+            if self.skip_current_song:
+                self.skip_current_song = False
+                self.lock.release()
+                continue
+            if self.skip_current_item:
+                self.skip_current_item = False
+                self.songs = []
+                self.lock.release()
+                continue
+            self.lock.release()
+
+            self.playing = True
+            self.player.play()
+            self.playing = False
+
+            self.lock.acquire()
+            self.skip_current_song = False
+            if self.skip_current_item:
+                self.skip_current_item = False
+                self.songs = []
+            self.lock.release()
+
+    @synchronised
     def pause(self):
+        if self.paused:
+            return
+        self.paused = True
         self.running.clear()
-        if self.player:
+        if self.playing:
             self.player.pause()
 
+    @synchronised
     def resume(self):
+        self.paused = False
         self.running.set()
-        if self.player:
+        if self.playing:
             self.player.resume()
 
-    def skip(self):
-        if self.player:
-            self.player.abort()
+    @synchronised
+    def skip_song(self):
+        if self.item:
+            self.skip_current_song = True
+            if self.playing:
+                self.player.abort()
+            if not self.running.is_set():
+                self.running.set()
 
+    @synchronised
     def skip_item(self):
         if self.item:
             self.skip_current_item = True
-            if self.player:
+            if self.playing:
                 self.player.abort()
+            if not self.running.is_set():
+                self.running.set()
 
+    @synchronised
     def is_playing(self):
-        return self.running.is_set()
+        return not self.paused
--- server/kakrafoond.py
+++ server/kakrafoond.py
@@ -139,7 +139,7 @@ def resume():
 
 @app.route('/skip', methods=['POST'])
 def skip():
-    control.skip()
+    control.skip_song()
     return ''
 
 @app.route('/skip_item', methods=['POST'])
