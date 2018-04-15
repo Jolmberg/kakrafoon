3528aa707857885f5696c279f2f3d571455093d0
Johannes Holmberg <johannes@update.uu.se>
2018-04-15 22:53:45 +0200

    Generate sha-256 hashes for songs when saving them

--- server/songpool.py
+++ server/songpool.py
@@ -1,20 +1,27 @@
+import hashlib
 import shutil
 import os
 from config import dictionary as config
 
 os.makedirs(config['song_pool_path'], exist_ok=True)
 
-def _save_song(item_id, song_id, fileobj):
+def _save_and_hash_song(item_id, song_id, fileobj):
     path = os.path.join(config['song_pool_path'], str(item_id))
     os.makedirs(path, exist_ok=True)
     filepath = os.path.join(path, str(song_id))
     fileobj.save(filepath)
+    hash = hashlib.sha256()
+    with open(filepath, 'rb') as f:
+        for x in iter(lambda: f.read(4096), b""):
+            hash.update(x)
+    return hash.hexdigest()
 
 
-def _save_songs(item):
+def _save_and_hash_songs(item):
     for song in item.songs:
-        _save_song(item.key, song.key, song.file_object)
+        hash = _save_and_hash_song(item.key, song.key, song.file_object)
         del song.file_object
+        song.hash = hash
         
 
 class SongPool(object):
@@ -29,7 +36,7 @@ class SongPool(object):
         """
         item_id = self.id_counter
         item.key = item_id
-        _save_songs(item)
+        _save_and_hash_songs(item)
         self.id_counter += 1
         self.items[item_id] = item
         return item_id
