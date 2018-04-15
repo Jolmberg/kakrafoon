fcd9bdd357eba1cfb91329217db9cf25cab4c714
Johannes Holmberg <johannes@update.uu.se>
2018-04-15 22:52:08 +0200

    Keep the start time untouched in stopwatches

--- server/stopwatch.py
+++ server/stopwatch.py
@@ -14,21 +14,21 @@ class StopWatch(object):
 
     def resume(self):
         if self.pause_time:
-            now = time.time()
-            self.start_time += now - self.pause_time
+            self.total_pause_time += time.time() - self.pause_time
             self.pause_time = None
 
     def read(self):
         if not self.start_time:
             return 0
         elif self.pause_time:
-            return self.pause_time - self.start_time
+            return self.pause_time - self.start_time - self.total_pause_time
         else:
-            return time.time() - self.start_time
+            return time.time() - self.start_time - self.total_pause_time
 
     def reset(self):
         self.start_time = None
         self.pause_time = None
+        self.total_pause_time = 0
 
 
 class StopWatchTest(unittest.TestCase):
@@ -41,8 +41,16 @@ class StopWatchTest(unittest.TestCase):
         w.resume()
         time.sleep(0.1)
         t = w.read()
-        self.assertGreater(t, 0.19)
-        self.assertLess(t, 0.21)
+        w.pause()
+        self.assertGreater(t, 0.18)
+        self.assertLess(t, 0.22)
+        t2 = w.read()
+        self.assertGreater(t2, t)
+        self.assertGreater(t2, 0.18)
+        self.assertLess(t2, 0.22)
+        time.sleep(0.1)
+        t3 = w.read()
+        self.assertEqual(t2, t3)
 
 if __name__ == '__main__':
     unittest.main()
